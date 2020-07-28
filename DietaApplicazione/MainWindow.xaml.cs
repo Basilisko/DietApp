844268179f3fa8d61b8c1282a61954a88aa5c622
Mirko <romanom89gmail.com>
2020-07-29 06:42:57 +0900

    Progetto nuovo

--- DietaApplicazione/MainWindow.xaml.cs
+++ DietaApplicazione/MainWindow.xaml.cs
@@ -13,15 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DietApp
-{
+namespace DietaApplicazione {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
     }
